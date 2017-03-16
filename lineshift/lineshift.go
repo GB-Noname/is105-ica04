d20aa73bc45bcefa5ed704c9e1c1555deb25f1ca
CastleDev <torgb15@uia.no>
2017-03-16 22:48:35 +0100

    Corrected some code, all works now -Tor

--- lineshift/lineshift.go
+++ lineshift/lineshift.go
@@ -9,36 +9,39 @@ func Tester(text1 []byte) string {
 	//text1 := "/GitHub/Org/is105-ica04/files/text1.txt"
 	//var a []byte = files.FileToByteslice(text1)
 	// 0a UTF8 linefeed
-	lf := "0a"
+	lf := 10
 	lfInt := 0
 	// 0d carriage return
-	cr := "0d"
+	cr := 13
 	crInt := 0
 	// 0c Form feed
-	ff := "0c"
+	ff := 12
 	ffInt := 0
+
 
 	var buffer bytes.Buffer
 	buffer.WriteString("Linjeendinger i teksten er: ")
 
 	for i := 0; i < len(text1); i++ {
-		fmt.Printf("%X", text1[i])
-		if string(text1[i]) == lf {
+		//fmt.Printf("%U", text1[i])
+		//fmt.Println(text1[i])
+		if int(text1[i]) == lf {
 			lfInt++
 		}
-		if string(text1[i]) == cr {
+		if int(text1[i]) == cr {
 			crInt++
 		}
-		if string(text1[i]) == ff {
+		if int(text1[i]) == ff {
 			ffInt++
 		}
 
 	}
-	if crInt >= lfInt {
-		buffer.WriteString("Carriage Return og LineFeed")
+	if crInt == lfInt {
+		buffer.WriteString("Carriage Return og LineFeed.")
+		buffer.WriteString("Dette betyr at det er fil laget med Windows/DOS system.")
 	}
 	if crInt < lfInt {
-		buffer.WriteString("Kun LineFeed")
+		buffer.WriteString("Kun LineFeed. Dette er en fil laget med UNIX/OSX system.")
 	}
 	fmt.Println("Antall LF:", lfInt)
 	fmt.Println("Antall CR:", crInt)
--- mainFiles.go
+++ mainFiles.go
@@ -6,34 +6,31 @@ import (
 	"reflect"
 
 	"./files"
-	//"./lineshift"
+	"./lineshift"
 	//"net/http"
 )
 
 func main() {
 
-	//files.FileToByteslice()
-	//
-	//text1, _ := filepath.Abs("//github.com/uia-worker/is105-ica04/tree/master/files/text1.txt")
-
-	//text1 := http.Get("https://gb-noname.github.io/text1.txt")
-	//text1 := "http://gb-noname.github.io/text1.txt"
 	//fmt.Printf("%s", files.FileToByteslice(text1))
-	//a := files.FileToByteslice("https://gb-noname.github.io/text1.txt")
 
 	urlText1 := "https://gb-noname.github.io/text1.txt"
 	urlText2 := "https://gb-noname.github.io/text2.txt"
 	a := files.FileToByteslice(urlText1)
 	b := files.FileToByteslice(urlText2)
 
+	//fmt.Printf("%x\n", a)
+	//fmt.Printf("%x\n", b)
 
-	//text2, _ := filepath.Abs("httpS//gb-noname.github.io/text1.txt")
-	fmt.Printf("%x\n", a)
-	fmt.Printf("%x\n", b)
-	//b := files.FileToByteslice(text2)
+	if reflect.DeepEqual(a, b) == false {
+		fmt.Println("Filene er ikke identiske")
+	} else {
+		fmt.Println("Filene er identiske")
+	}
+	//fmt.Print(a)
 
-	fmt.Println(reflect.DeepEqual(a, b))
-	fmt.Printf("%s", a)
+	fmt.Println(lineshift.Tester(a))
+	fmt.Println(lineshift.Tester(b))
 	//fmt.Println(reflect.DeepEqual(text1, text2))
 
 	//fmt.Printf("% x\n", files.FileToByteslice(text1))
