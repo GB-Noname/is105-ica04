0521480385e1c73619db85efa07856ec36a023c4
Tor Borgen <Tor Borgen>
2017-03-16 15:50:21 +0100

    222@

--- mainFiles.go
+++ mainFiles.go
@@ -12,11 +12,12 @@ func main() {
 
 	//files.FileToByteslice()
 	//
-	text1, _ := filepath.Abs("/Users/Ann Margrethe/Documents/GitHub/GB-noname/is105-ica04/files/text1.txt")
+	//text1, _ := filepath.Abs("//github.com/uia-worker/is105-ica04/tree/master/files/text1.txt")
+	text1 := "//github.com/GB-Noname/is105-ica04/blob/master/files/text1.txt"
 	fmt.Printf("%s", files.FileToByteslice(text1))
 	a := files.FileToByteslice(text1)
 	//
-	text2, _ := filepath.Abs("/Users/Ann Margrethe/Documents/GitHub/GB-noname/is105-ica04/files/text2.txt")
+	text2, _ := filepath.Abs("//github.com/uia-worker/is105-ica04/tree/master/files/text2.txt")
 	fmt.Printf("%s", files.FileToByteslice(text2))
 	b := files.FileToByteslice(text2)
 
