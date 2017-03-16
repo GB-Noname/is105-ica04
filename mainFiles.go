d0b973862616a1005672c1ef5fdd33afa522d5dd
ampede15 <ampede15@uia.no>
2017-03-16 15:17:13 +0100

    begynnelse på 1a

---
+++ mainFiles.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"reflect"
+
+	"./files"
+)
+
+func main() {
+
+	//files.FileToByteslice()
+	//
+	text1, _ := filepath.Abs("/Users/Ann Margrethe/Documents/GitHub/GB-noname/is105-ica04/files/text1.txt")
+	fmt.Printf("%s", files.FileToByteslice(text1))
+	a := files.FileToByteslice(text1)
+	//
+	text2, _ := filepath.Abs("/Users/Ann Margrethe/Documents/GitHub/GB-noname/is105-ica04/files/text2.txt")
+	fmt.Printf("%s", files.FileToByteslice(text2))
+	b := files.FileToByteslice(text2)
+
+	fmt.Println(reflect.DeepEqual(a, b))
+	fmt.Println(reflect.DeepEqual(text1, text2))
+
+	fmt.Printf("%x\n", files.FileToByteslice(text1))
+	fmt.Printf("%x", files.FileToByteslice(text2))
+
+}
