ace47f50212210353ea3a8a155ce0c6b28198877
Tor Borgen <Tor Borgen>
2017-03-20 14:59:24 +0100

    Fixed the code, working

--- fileinfo/fileinfo.go
+++ fileinfo/fileinfo.go
@@ -5,21 +5,19 @@ import (
 	"os"
 )
 
+var file os.FileInfo
+
 func main() {
+	arg := os.Args[1]
 
-	file, err := os.Open("fileinfo.go")
-	if err != nil {
-		return
-	}
-	defer file.Close()
 
-	stat, err := file.Stat()
-	if err != nil {
-		return
-	}
+	file, _ := os.Stat(arg)
+
+
+
 
 	var bytes int64
-	bytes = stat.Size()
+	bytes = file.Size()
 
 	var kilobytes int64
 	kilobytes = (bytes / 1024)
@@ -42,6 +40,13 @@ func main() {
 	var zettabytes float64
 	zettabytes = (exabytes / 1024)
 
+	fmt.Println("File name:", file.Name())        //..
+	fmt.Println("Size in bytes:", file.Size())    //Størrelse i bytes
+	fmt.Println("Permissions:", file.Mode())      //..
+	fmt.Println("Last modified:", file.ModTime()) //Sist endret
+	fmt.Println("Is Directory: ", file.IsDir())   //..
+
+
 	fmt.Println("File size in bytes ", bytes)
 	fmt.Println("File size in kilobytes ", kilobytes)
 	fmt.Println("File size in megabytes ", megabytes)
@@ -49,7 +54,7 @@ func main() {
 	fmt.Println("File size in terabytes ", terabytes)
 	fmt.Println("File size in petabytes ", petabytes)
 	fmt.Println("File size in exabytes ", exabytes)
-
 	fmt.Println("File size in zettabytes ", zettabytes)
+
 
 }
